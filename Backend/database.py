491b2c19d9c4f362544aecfddeef569b53dcb12a
Saurabh Kushwaha <kushwahasaurabh639@gmail.com>
2026-04-21 11:40:23 +0530

    converted to sqlite

--- Backend/database.py
+++ Backend/database.py
@@ -1,4 +1,4 @@
-﻿import mysql.connector
+﻿'''import mysql.connector
 
 conn = mysql.connector.connect(
     host="localhost",
@@ -6,4 +6,19 @@ conn = mysql.connector.connect(
     password="saurabh@123",
     database="health_db"
 )
-cursor = conn.cursor()
+cursor = conn.cursor()'''
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./health.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
