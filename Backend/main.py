6e3c783f8a45ff19803e7efd95a5add1382abccc
Saurabh Kushwaha <kushwahasaurabh639@gmail.com>
2026-04-22 11:26:09 +0530

    fixed cors issue

--- Backend/main.py
+++ Backend/main.py
@@ -11,10 +11,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-    ],
+    allow_origins=["*"],  # allow all (for testing)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
